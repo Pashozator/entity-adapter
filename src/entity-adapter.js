907144fee2083194b0cea547fa441a49a4d9969d
Wojciech Szućko <wojciech.szucko@gmail.com>
2019-03-22 22:29:23 +0100

    feat: add remove one method

--- src/entity-adapter.js
+++ src/entity-adapter.js
@@ -8,4 +8,10 @@ export class EntityAdapter {
         this.ids.push(element.id);
         this.entities[element.id] = element;
     }
+
+    removeOne(id) {
+        delete this.entities[id];
+
+        this.ids = this.ids.filter(_id => _id !== id);
+    }
 }
--- src/entity-adapter.test.js
+++ src/entity-adapter.test.js
@@ -20,4 +20,14 @@ describe('Entity adapter', () => {
         expect(entityAdapter.ids[0]).toBe(element.id);
         expect(entityAdapter.entities[element.id]).toEqual(element);
     });
+
+    it('should remove element', () => {
+        const element = { id: '1', label: 'first' };
+
+        entityAdapter.addOne(element);
+        entityAdapter.removeOne(element.id);
+
+        expect(entityAdapter.ids.length).toBe(0);
+        expect(entityAdapter.entities).toEqual({});
+    })
 });
